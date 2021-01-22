8802235ae56aefd559ccd6da9d65b91159958bea
username <maximka5406@gmail.com>
2021-01-22 23:14:44 +0300

    fix2

--- sources/experiment.cpp
+++ sources/experiment.cpp
@@ -1,10 +1,8 @@
 // Copyright 2020 Your Name <your_email>
 
-#include <experiment.hpp>
-
-#include <experiment.hpp>
 #include <chrono>
 #include <config.hpp>
+#include <experiment.hpp>
 #include <stdexcept>
 #include <string>
 
@@ -25,13 +23,13 @@ std::size_t Experiment::Run(Experiment::TravelOrder order) {
     if (order == Direct) {
       std::size_t index{0};
       for (std::size_t j = 0; j < bufferSize; j += CacheLineSize) {
-        index += CacheLineSize;
         auto start = std::chrono::high_resolution_clock::now();
         k = buffer[index];
         auto stop = std::chrono::high_resolution_clock::now();
         commonDuration +=
             std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
                 .count();
+        index += CacheLineSize;
       }
     } else if (order == Reverse) {
       std::size_t index{bufferSize};
