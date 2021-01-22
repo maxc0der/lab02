5d43286e69520a0cfaa39521ce72d81b4f130503
username <maximka5406@gmail.com>
2021-01-22 22:56:25 +0300

    fix

--- include/config.hpp
+++ include/config.hpp
@@ -8,5 +8,5 @@
 
 const std::size_t IterationsCount = 1000;
 const std::size_t CacheLineSize = 64;
-const std::vector<std::size_t> CacheSizes = {128 * 1024, 1024 * 1024, 32 * 1024 * 1024};
+const std::vector<std::size_t> CacheSizes = {128 * 1024, 256 * 1024, 32 * 1024 * 1024};
 #endif  // LAB02_CONFIG_HPP
